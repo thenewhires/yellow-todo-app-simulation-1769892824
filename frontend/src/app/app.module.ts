cc67566fe5e31058a6a10180e531dbd6091f779e
thenewhires <the.new.hires.public@gmail.com>
2026-01-31 21:53:51 +0100

    Add frontend/src/app/app.module.ts

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { TodoService } from './todo.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [TodoService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
